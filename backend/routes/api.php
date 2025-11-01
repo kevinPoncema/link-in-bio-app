f7957c076b3212c4776cf5ce8ff7c71d7c842849
kevin@ponce <kevin.ponce@alumnos.uneatlantico.es>
2025-11-01 21:18:53 +0100

    Fix route definition for links resource and adjust profile links endpoint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,7 +14,8 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::apiResource('/profiles', ProfileController::class)->only(['show']);
-Route::apiResource('/profiles.links', LinkController::class)->only(['index', 'show']);
+Route::apiResource('/links', LinkController::class)->only(['show']);
+Route::get('/profiles/{profile}/links', [LinkController::class, 'index']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
